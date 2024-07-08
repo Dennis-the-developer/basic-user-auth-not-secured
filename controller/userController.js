eef5f103a38a0300036584c51376f1efcfa15224
Dennis Appah <dev.dennisappah@gmail.com>
2024-07-08 05:12:01 +0000

    Fixed flaws in email format, password length and (username starting numbers and usernames with less than 8 characters

--- controller/userController.js
+++ controller/userController.js
@@ -1,19 +1,51 @@
-import { userModel } from "../model/userModel.js"; 
+import { userModel } from "../model/userModel.js";
+import mailchecker from 'mailchecker';
+
+const checkEmailFormat = mailchecker;
 
 //  USER SIGNUP
 export const signupUser = async (req, res, next) => {
     try {
         const { email, password, username } = req.body;
-        if (username === "" || username == null || username == undefined){
-            res.status(400).json("Username cannot be empty");
-        } else {
-            const newUser = await userModel.create({ email, password, username }); 
-            res.status(201).json('User created successfully');
+        // SIGNUP CREDENTIAL VALIDATION
+          // Check if Email is formatted properly
+        if (checkEmailFormat.isValid(email)) {
+            // Check if Password characters are 8 or more
+            if (password.length < 8) {
+                res.status(400).json('Password should have a minimum of 8 charaters');
+                return;
+            }
+            // Check if username is having characters
+            if (username === "" || username == null || username == undefined) {
+                res.status(400).json("Username cannot be empty");
+                return;
+            // check if username is not starting with a number
+            } else if (!isNaN(username[0])) { 
+                res.status(400).json('Username cannot begin with a number');
+                return;
+            // Check if username length is 6 or more
+            } else if(username.length < 6) {
+                res.status(400).json('Username characters should be 6 or more');
+                return;
+            }
+            else {
+                // Creating account after successful checks
+                const newUser = await userModel.create({ email, password, username });
+                res.status(201).json('User created successfully');
+                return;
+            }
         }
-         
+         // Tell user to check and correct email format
+        else {
+            res.status(400).json(`${email} not formated properly. Check email again`);
+            return;
+        }
     } catch (error) {
-        if (error.code === 11000) { // duplicate key error (User Already exist)
-            res.status(400).json('User already exists'); // Responed with user already exist
+        // Check for duplicate key error (User Already exist)
+        if (error.code === 11000) {
+            // Responed with user already exist
+            res.status(400).json('User already exists');
+            return;
         } else {
             next(error);
         }
@@ -24,14 +56,18 @@ export const signupUser = async (req, res, next) => {
 export const loginUser = async (req, res, next) => {
     try {
         const { email, password } = req.body;
-        const user = await userModel.findOne({ email }); 
-        if (!user || user.password !== password) { // User not existing or password mismatch
-            return res.status(401).json('Invalid email or password'); 
+        // Find user in database using user-email
+        const user = await userModel.findOne({ email });
+        // User not existing or password mismatch
+        if (!user || user.password !== password) { 
+            res.status(401).json('Invalid email or password');
+            return;
         }
         else {
             res.status(200).json(`Login successful \n Welcome ${user.username}`);
+            return;
         }
     } catch (error) {
-        next(error); 
+        next(error);
     }
 };
